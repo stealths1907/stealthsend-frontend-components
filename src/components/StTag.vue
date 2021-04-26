bbd163a60437a222349ca8e95201d0028427e69d
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-04-26 10:53:02 +0200

    XST-165 install husky

--- src/components/StTag.vue
+++ src/components/StTag.vue
@@ -15,7 +15,7 @@ export default {
         return 'success'
       },
       validator: (value) => {
-        ['success'].includes(value)
+        return ['success'].includes(value)
       }
     }
   },

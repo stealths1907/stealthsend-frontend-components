b6f63c47039ef9bcc4d308bc05c59691e98f74c2
Matej Lazrevic <matej.lazarevic@barrage.net>
2021-05-10 11:44:04 +0200

    switch component

--- src/components/StSwitch.vue
+++ src/components/StSwitch.vue
@@ -1,38 +1,53 @@
 <template>
-    <div class="st-switch"  :class="{'st-switch--is-off': !isOn}" @click="toggle">
+  <div
+    class="st-switch"
+    :class="{ 'st-switch--is-off': !isOn }"
+    @click="toggle"
+  >
     <div class="st-switch__label">
-        <svg width="9" height="12" viewBox="0 0 9 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5l1-5-6 7h3l-1 5 6-7h-3z" :fill="[isOn ? '#E0D3FC' : '#4E00F6']"/></svg>
+      <svg
+        width="9"
+        height="12"
+        viewBox="0 0 9 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.5 5l1-5-6 7h3l-1 5 6-7h-3z"
+          :fill="[isOn ? '#E0D3FC' : '#4E00F6']"
+        />
+      </svg>
     </div>
   </div>
 </template>
 
 <script>
-import {ref} from 'vue'
-    export default {
-        name: 'StSwitch',
-        emits: ['input'],
-        setup(_, ctx) {
-            const isOn = ref(false)
-            function toggle() {
-                isOn.value = !isOn.value
-                ctx.emit('input', isOn.value)
-            }
-
-            return {
-                toggle,
-                isOn
-            }
-        }
+import { ref } from 'vue'
+export default {
+  name: 'StSwitch',
+  emits: ['input'],
+  setup(_, ctx) {
+    const isOn = ref(false)
+    function toggle() {
+      isOn.value = !isOn.value
+      ctx.emit('input', isOn.value)
     }
+
+    return {
+      toggle,
+      isOn
+    }
+  }
+}
 </script>
 
 <style scoped>
 .st-switch {
   cursor: pointer;
   -webkit-user-select: none;
-     -moz-user-select: none;
-      -ms-user-select: none;
-          user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
   background: transparent;
   width: 60px;
   border: 1.5px solid var(--grey200);
@@ -52,9 +67,8 @@ import {ref} from 'vue'
   /* align-self: flex-end; */
   transform: translateX(33px);
 
-
-    margin-top: 1px;
-    margin-right: 1px;
+  margin-top: 1px;
+  margin-right: 1px;
 }
 .st-switch__label {
   width: 24px;
@@ -66,10 +80,10 @@ import {ref} from 'vue'
   color: #fff;
   border-radius: 50%;
 
-      margin-top: 1px;
-    margin-left: 1px;
-        display: flex;
-    align-items: center;
-    justify-content: center;
+  margin-top: 1px;
+  margin-left: 1px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
